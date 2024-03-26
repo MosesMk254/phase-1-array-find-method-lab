3420dfe00081c726e5cda480701258de50fb087f
Moses Mutisya <mutisyamoses722@gmail.com>
2024-03-26 13:58:25 +0300

    testing the .find() method on arrays

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(records) {
+  const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+
+  const winRecord = records.find((record) => record.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
